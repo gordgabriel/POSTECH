a2a017ebce000e3fd733e3f9ce04b0b777e0139f
Gabriel alberto bento <gabriel.bento@cliqx.com.br>
2026-07-13 11:26:45 -0300

    First commit

--- So_PosTech/urls.py
+++ So_PosTech/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     # Swagger / OpenAPI
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui-alt'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     # API routes
